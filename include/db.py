7de62028c2ae08d54657201e589ffbd755a8eb8b
oopsfoo <oopsfoo@users.noreply.github.com>
2021-03-10 19:34:11 -0800

    refactor db.py

--- include/db.py
+++ include/db.py
@@ -1,60 +1,177 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-class VarTable:
-
-    def __init__(self):
-        self.table={}
-    
-    def hasKey(self, key):
-        return key in self.table 
-
-    def getValue(self, key):
-        return self.table.get(key, None) 
-
-    def put(self, key, value):
-        self.table[key] = value 
-
-    def dump(self):
-        for var in self.table.keys():
-            print('%s := %s' % (var, self.getValue(var)))
-
-    def reset(self):
-        self.table = {}
+var_table = {}
+rule_table = {}
 
 
-class RuleTable:
+def has_var(var: str):
+    return var in var_table
 
-    def __init__(self):
-        self.table={}
-    
-    def addDeps(self, target, depTarget):
-        rule = self.getByTarget(target)
-        rule['deps'].append(depTarget)
-    
-    def getByTarget(self, target):
-        if target not in self.table:
-            self.table[target] = {'target': target, 'deps':[]}
-        return self.table[target]
 
-    def printRule(self, target):
-        targetStr = ''
-        if target in self.table:
-            rule = self.table[target]
-            targetStr += rule['target']
-            targetStr += ':'
-            for depTarget in rule['deps']:
-                targetStr += ' '
-                targetStr += depTarget
-        print(targetStr)
-        print('')
+def get_val(var: str):
+    return var_table.get(var, None)
 
-    def dump(self):
-        for target in self.table.keys():
-            self.printRule(target)
 
-    def reset(self):
-        self.table = {}
+def set_var(var, val):
+    var_table[var] = val
 
-varTable = VarTable()
-ruleTable = RuleTable()
+
+def dump_all_vars():
+    for var in var_table.keys():
+        print('%s := %s' % (var, get_val(var)))
+
+
+def empty_vars():
+    global var_table
+    var_table = {}
+
+
+def add_rule(target: str) -> None:
+    global rule_table
+    rule_table[target] = {
+        'target': target,
+        'prerequisites': [],
+        'recipes': []
+    }
+
+
+def rule_by_target(target: str, creat: bool = True) -> dict:
+    global rule_table
+    if target not in rule_table:
+        if creat is False:
+            return None
+        add_rule(target)
+    return rule_table[target]
+
+
+def add_prerequisites(target: str, *prerequisites: str) -> None:
+    rule = rule_by_target(target, True)
+    for prerequisite in prerequisites:
+        rule['prerequisites'].append(prerequisite)
+
+
+def get_prerequisites(target: str) -> list:
+    rule = rule_by_target(target, False)
+    if rule is not None:
+        return rule['prerequisites']
+
+
+def add_recipes(target: str, *recipes: str) -> None:
+    rule = rule_by_target(target, True)
+    for recipe in recipes:
+        rule['recipes'].append(recipe)
+
+
+def get_recipes(target: str) -> list:
+    rule = rule_by_target(target, False)
+    if rule is not None:
+        return rule['recipes']
+
+
+def empty_rules():
+    global rule_table
+    rule_table = {}
+
+
+def dump_rule(target: str) -> None:
+    dump_str = target + ': '
+    rule = rule_by_target(target, False)
+    if rule is None:
+        return
+    for prerequisite in get_prerequisites(target):
+        dump_str += prerequisite + ' '
+    dump_str += '\n'
+    for recipe in get_recipes(target):
+        dump_str += '\t%s\n' % recipe
+    print(dump_str, '\n')
+
+
+def dump_rules(*targets: str) -> None:
+    for target in targets:
+        dump_rule(target)
+
+
+def dump_all_rules():
+    global rule_table
+    for target in rule_table.keys():
+        dump_rule(target)
+
+
+def reset():
+    empty_vars()
+    empty_rules()
+
+
+def dump():
+    dump_all_vars()
+    dump_all_rules()
+
+
+def test_var():
+    set_var('var1', 'val1')
+    print('1=====================')
+    print('var1 = %s' % get_val('var1'))
+    print('var1 exist = %s' % has_var('var1'))
+    print('var2 = %s' % get_val('var2'))
+    print('var2 exist = %s' % has_var('var2'))
+    print('var3 = %s' % get_val('var3'))
+    print('var3 exist = %s' % has_var('var3'))
+    dump_all_vars()
+
+    set_var('var2', 'val2')
+    print('2=====================')
+    print('var1 = %s' % get_val('var1'))
+    print('var1 exist = %s' % has_var('var1'))
+    print('var2 = %s' % get_val('var2'))
+    print('var2 exist = %s' % has_var('var2'))
+    print('var3 = %s' % get_val('var3'))
+    print('var3 exist = %s' % has_var('var3'))
+    dump_all_vars()
+
+    set_var('var3', 'var3')
+    print('2=====================')
+    print('var1 = %s' % get_val('var1'))
+    print('var1 exist = %s' % has_var('var1'))
+    print('var2 = %s' % get_val('var2'))
+    print('var2 exist = %s' % has_var('var2'))
+    print('var3 = %s' % get_val('var3'))
+    print('var3 exist = %s' % has_var('var3'))
+    dump_all_vars()
+
+    print('ALL#####################')
+    dump_all_vars()
+
+    print('OVERWRITE#####################')
+    set_var('var3', 'var4')
+    dump_all_vars()
+
+    print('CLEAR#####################')
+    empty_vars()
+    dump_all_vars()
+
+
+def test_rule():
+    add_prerequisites('target1', 'pre1', 'pre2')
+    add_recipes('target1', 'do sth1', 'do sth2')
+    print('1=====================')
+    dump_rules('target1', 'target2', 'target3')
+
+    add_prerequisites('target2', 'pre1', 'pre2')
+    print('2=====================')
+    dump_rules('target1', 'target2', 'target3')
+
+    add_rule('target3')
+    print('3=====================')
+    dump_rules('target1', 'target2', 'target3')
+
+    print('ALL#####################')
+    dump_all_rules()
+
+    print('CLEAR#####################')
+    empty_rules()
+    dump_all_rules()
+
+
+if __name__ == '__main__':
+    test_var()
+    test_rule()
--- include/subdir.py
+++ include/subdir.py
@@ -3,45 +3,47 @@
 
 import db
 
-def subtarget_default(workdir, target):
-    target_key = '%s/builddirs-%s'%(workdir, target)
-    default_key = '%s/builddirs-default' % workdir
-    the_key = '%s/builddirs' % workdir
+
+def subtarget_default(subdir: str, target: str):
+    target_key = '%s/builddirs-%s' % (subdir, target)
+    default_key = '%s/builddirs-default' % subdir
+    the_key = '%s/builddirs' % subdir
 
     scan_keys = [target_key, default_key, the_key]
 
     for key in scan_keys:
-        if db.varTable.hasKey(key):
+        if db.has_var(key):
             subtargets = []
-            for subtarget in db.varTable.getValue(key):
+            for subtarget in db.get_val(key):
                 if subtarget is not '.':
                     subtargets.append(subtarget)
             return subtargets
 
-def subtarget(workdir, target):
-    target_name = '%s/%s' % (workdir, target)
-    for subtarget in subtarget_default(workdir, target):
-        db.ruleTable.addDeps(target_name, '%s/%s/%s' % (workdir, subtarget, target))
+
+def subtarget(subdir, target):
+    target_name = '%s/%s' % (subdir, target)
+    for subtarget in subtarget_default(subdir, target):
+        db.add_prerequisites(target_name, '%s/%s/%s' % (subdir, subtarget, target))
 
 
 def subdir(workdir):
     build_dirs = '%s/builddirs' % workdir
 
-    default_subtargets = db.varTable.getValue('SUBTARGETS')
-    workdir_subtargets = db.varTable.getValue('%s/subtargets' % workdir)
+    default_subtargets = db.get_val('SUBTARGETS')
+    workdir_subtargets = db.get_val('%s/subtargets' % workdir)
     subtargets = vars_join(default_subtargets, workdir_subtargets)
 
-    for bd in db.varTable.getValue(build_dirs):
+    for bd in db.get_val(build_dirs):
         for target in subtargets:
-            pass #64-79
+            pass  # 64-79
 
     for target in subtargets:
         subtarget(workdir, target)
 
 
 def stampfile(subdir, name, target, depends, config_options, stampfile_location):
-    staging_dir = db.varTable.getValue('STAGING_DIR')
-    tmp_dir = db.varTable.getValue('TMP_DIR')
+    staging_dir = db.get_val('STAGING_DIR')
+    tmp_dir = db.get_val('TMP_DIR')
 
     #   $(1)/stamp-$(3):=$(if $(6),$(6),$(STAGING_DIR))/stamp/.$(2)_$(3)$(5)
     stamp_var = '%s/stamp-%s' % (subdir, target)
@@ -51,32 +53,32 @@ def stampfile(subdir, name, target, depends, config_options, stampfile_location)
     else:
         stamp_val += stampfile_location
     stamp_val += '/stamp/.%s_%s%s' % (name, target, config_options)
-    db.varTable.put(stamp_var, stamp_val)
+    db.set_var(stamp_var, stamp_val)
 
     #   $$($(1)/stamp-$(3)): $(TMP_DIR)/.build $(4)
     #       @+$(SCRIPT_DIR)/timestamp.pl -n $$($(1)/stamp-$(3)) $(1) $(4) || \
     #       $(MAKE) $(if $(QUIET),--no-print-directory) $$($(1)/flags-$(3)) $(1)/$(3)
     #       @mkdir -p $$$$(dirname $$($(1)/stamp-$(3)))
     #       @touch $$($(1)/stamp-$(3))
-    db.ruleTable.addDeps(stamp_val, '%s/.build %s' % (tmp_dir, depends))
-    #TODO db.ruleTable.addCmds(clean_target, 'script/timestamp.pl' )
-    #TODO db.ruleTable.addCmds(clean_target, 'make' )
-    #TODO db.ruleTable.addCmds(clean_target, 'mkdir' )
-    #TODO db.ruleTable.addCmds(clean_target, 'touch' )
+    db.add_prerequisites(stamp_val, '%s/.build %s' % (tmp_dir, depends))
+    # TODO db.addCmds(clean_target, 'script/timestamp.pl' )
+    # TODO db.addCmds(clean_target, 'make' )
+    # TODO db.addCmds(clean_target, 'mkdir' )
+    # TODO db.addCmds(clean_target, 'touch' )
 
-    #TODO   $$(if $(call debug,$(1),v),,.SILENT: $$($(1)/stamp-$(3)))
-    #TODO   .PRECIOUS: $$($(1)/stamp-$(3)) # work around a make bug 
-    
+    # TODO   $$(if $(call debug,$(1),v),,.SILENT: $$($(1)/stamp-$(3)))
+    # TODO   .PRECIOUS: $$($(1)/stamp-$(3)) # work around a make bug
+
     #   $(1)//clean:=$(1)/stamp-$(3)/clean
     clean_var = '%s//clean' % subdir
     clean_val = '%s/stamp-%s/clean' % (subdir, target)
-    db.varTable.put(clean_var, clean_val)
+    db.set_var(clean_var, clean_val)
 
     #   $(1)/stamp-$(3)/clean: FORCE
     #       @rm -f $$($(1)/stamp-$(3))
     clean_target = '%s/clean' % stamp_var
-    db.ruleTable.addDeps(clean_target, 'FORCE')
-    #TODO db.ruleTable.addCmds(clean_target, '@rm -f %s' %)
+    db.add_prerequisites(clean_target, 'FORCE')
+    # TODO db.addCmds(clean_target, '@rm -f %s' %)
 
 
 def vars_join(a, b):
@@ -87,26 +89,27 @@ def vars_join(a, b):
         s += b
     return s
 
+
 def test_subdir():
-    db.varTable.reset()
-    db.ruleTable.reset()
-    db.varTable.put('SUBTARGETS', ['clean', 'download', 'prepare', 'compile', 'update', 'refresh', 'prereq', 'dist', 'distcheck', 'configure', 'check', 'check-depends'])
-    db.varTable.put('target/subtargets', ['install'])
-    db.varTable.put('target/builddirs', ['linux', 'sdk', 'imagebuilder', 'toolchain'])
-    db.varTable.put('target/builddirs-default', ['linux'])
-    db.varTable.put('target/builddirs-install', ['linux', 'sdk', 'imagebuilder', 'toolchain'])
+    db.reset()
+    db.set_var('SUBTARGETS',
+               ['clean', 'download', 'prepare', 'compile', 'update', 'refresh', 'prereq', 'dist', 'distcheck',
+                'configure', 'check', 'check-depends'])
+    db.set_var('target/subtargets', ['install'])
+    db.set_var('target/builddirs', ['linux', 'sdk', 'imagebuilder', 'toolchain'])
+    db.set_var('target/builddirs-default', ['linux'])
+    db.set_var('target/builddirs-install', ['linux', 'sdk', 'imagebuilder', 'toolchain'])
     subdir('target')
-    db.ruleTable.dump()
+    db.dump()
+
 
 def test_stampfile():
-    db.varTable.reset()
-    db.ruleTable.reset()
-    db.varTable.put('STAGING_DIR', '$ROOT/staging_dir/target-x86_64_musl')
-    db.varTable.put('TMP_DIR', '$ROOT/tmp')
+    db.reset()
+    db.set_var('STAGING_DIR', '$ROOT/staging_dir/target-x86_64_musl')
+    db.set_var('TMP_DIR', '$ROOT/tmp')
     stampfile('target', 'target', 'compile', 'tmp/.build', '', '')
-    db.varTable.dump()
-    print('---------------------------')
-    db.ruleTable.dump()
+    db.dump()
+
 
 if __name__ == '__main__':
     print('#### subdir ####\n')
